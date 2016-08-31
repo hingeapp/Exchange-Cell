3b4732cc3a03a05b48d40dd2b1c2279b8104b8cf
AdityaShirole <adityacr7shirole@gmail.com>
2016-08-31 14:33:53 +0530

    initial commit

---
+++ app/src/main/java/io/github/hingeapp/exchangecell/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.hingeapp.exchangecell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
